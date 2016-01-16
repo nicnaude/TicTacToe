2e81a3202100096b6919bcb5c98a882c3ffe6091
nicholasnaude <nicholas.naude@icloud.com>
2016-01-16 09:41:49 -0800

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Nicholas Naudé on 16/01/2016.
+//  Copyright © 2016 Nicholas Naudé. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Nicholas Naudé on 16/01/2016.
+//  Copyright © 2016 Nicholas Naudé. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
